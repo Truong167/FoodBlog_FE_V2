43e10ab23285cd1e053f8f0ffa961463795aafa5
Truong711 <vietruong7111@gmail.com>
2025-06-22 21:26:06 +0700

    feat/tacman: update body for release 70

--- .versionrc.js
+++ .versionrc.js
@@ -1,6 +1,5 @@
+const path = require("path");
 module.exports = {
-  // Chỉ định đường dẫn đến preset tùy chỉnh của bạn
-  preset: "./changelog-config/index.js",
-  // Các tùy chỉnh khác cho standard-version
-  // ví dụ: skip.changelog = true (nếu bạn muốn bỏ qua việc tạo changelog)
+  preset: path.resolve(__dirname, "changelog-config/index.js"),
+  // ...
 };
